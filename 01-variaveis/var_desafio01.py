7e904ece764d50f4b778223d61d3c5b78e14a5ec
mariaclarachinaideesilva2010-bit <mariaclarachinaideesilva2010@gmail.com>
2026-08-13 21:29:16 -0300

    variáveis inteiras

---
+++ 01-variaveis/var_desafio01.py
@@ -0,0 +1,6 @@
+# Autor: Maria Clara
+# Projeto: Desafio 01
+largura = 12
+comprimento = 22
+area = comprimento * largura
+print(area)
---
+++ 01-variaveis/var_int.py
@@ -0,0 +1,23 @@
+# Autor: Maria Clara
+# Projeto: Estudando variáveis inteiras
+# Nome de variavel nao pode ter caracteres especiais 
+# 
+# nome e tipo
+valor1 = 12
+valor2 = 22
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+
+print('A soma é: ', soma)
+print('A subtracao é: ', subtracao)
+print('A multiplicacao é: ', multiplicacao)
+print('A divisao é: ', divisao)
+
+# Imprimindo todos os resultados em uma única linha
+print('A soma é: ', soma, 'e a subtracao é: ', subtracao)
