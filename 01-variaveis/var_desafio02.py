af5e71b1ef5143d450e4923ac933aa29a09d453e
mariaclarachinaideesilva2010-bit <mariaclarachinaideesilva2010@gmail.com>
2026-08-19 21:15:43 -0300

    Add files via upload

---
+++ 01-variaveis/var_desafio02.py
@@ -0,0 +1,9 @@
+# Autor: Maria Clara
+# Projeto: Criar um sistema que faça a conversão de reais para dolar
+
+real = float(input('Digite o valor em reais: '))
+cotaçao = 5.18
+dolar = real / cotaçao
+print (f'Voce tem US$ {dolar}')
+
+
