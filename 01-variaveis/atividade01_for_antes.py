7758e5cb78ed835d4d47b4aa795e1b140575ed78
mariaclarachinaideesilva2010-bit <mariaclarachinaideesilva2010@gmail.com>
2026-08-26 21:41:39 -0300

    Add files via upload

---
+++ 01-variaveis/atividade01_for_antes.py
@@ -0,0 +1,26 @@
+# Autor: Maria Clara
+# Projeto: Aprendendo loop FOR
+
+# Entendendo a lógica
+# Contar 10 números
+a=1
+b=2
+c=3
+d=4
+e=5
+f=6
+g=7
+h=8
+i=9
+j=10
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
